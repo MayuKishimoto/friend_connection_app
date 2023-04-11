51b1e4ffa6c0383b90b343642b20bf7807ba9f46
mayu_kishimoto <nhpw.29@gmail.com>
2023-04-11 09:53:46 +0900

    ルートへのルーティング設定の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :feeds do
-    collection do #member doを使うと、idを必要とする固有のルーティングを生成できます。
+    collection do
       post :confirm
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  root :to => 'users#new'
 end
