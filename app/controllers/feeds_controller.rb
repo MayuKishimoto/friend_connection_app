953fefebce9e146d4fa01196a58da15a51f8b33d
mayu_kishimoto <nhpw.29@gmail.com>
2023-04-11 16:51:54 +0900

    feeds_controllerの不要なコメントを削除

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,16 +1,12 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: %i[ show edit update destroy ]
-
-  # GET /feeds or /feeds.json
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1 or /feeds/1.json
   def show
   end
 
-  # GET /feeds/new
   def new
     if params[:back]
       @feed = Feed.new(feed_params)
@@ -19,11 +15,9 @@ class FeedsController < ApplicationController
     end
   end
 
-  # GET /feeds/1/edit
   def edit
   end
 
-  # POST /feeds or /feeds.json
   def create
     @feed = current_user.feeds.build(feed_params)
     respond_to do |format|
@@ -37,7 +31,6 @@ class FeedsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /feeds/1 or /feeds/1.json
   def update
     respond_to do |format|
       if @feed.update(feed_params)
@@ -50,7 +43,6 @@ class FeedsController < ApplicationController
     end
   end
 
-  # DELETE /feeds/1 or /feeds/1.json
   def destroy
     @feed.destroy
 
@@ -66,12 +58,10 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_feed
       @feed = Feed.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def feed_params
       params.require(:feed).permit(:image, :content, :image_cache)
     end
